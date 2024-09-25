b95e5cd1fe92cc11c91aeeb20d44c8ec46ce8d64
FoF312 <bulonv786@gmail.com>
2024-09-25 21:10:04 +0200

    Update main.cpp

--- Work2/main.cpp
+++ Work2/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
+
 using namespace std;
 
 int main() {
-   double R;
-   cout<<"Enter the radius of the circle( 0 to 100):";
-   cin >> R;
-   
-
+    double R;
+    const double PI = acos(-1.0);
+    cout << "Enter the radius of the circle( 0 to 100):";
+    cin >> R;
+    if (R <= 0 || R > 100)
+    {
+        cout << "Error! the number doesn't fit";
+        return 1;
+    }      
+        double area = PI * R * R;
+    
+    cout << fixed << setprecision(9);
+    cout << "Area of a circle with radius " << R << "  equal  " << area << endl;
     system("pause");
     return 0;
 }
