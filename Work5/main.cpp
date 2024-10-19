72e3e759dbeb376d94072d197f16bd73669dae82
FoF312 <bulonv786@gmail.com>
2024-10-19 10:35:17 +0200

    Update main.cpp

--- Work5/main.cpp
+++ Work5/main.cpp
@@ -3,27 +3,31 @@
 using namespace std;
 
 int main() {
-
+  
     //Задание 1
     const short zero = 0;
     const short seven = 7;
     const int MassiveLenght = 6;
-    int massive[MassiveLenght] = { 20,37,80,32,64,107 };
+    int massive[MassiveLenght] = {20,37,80,32,64,107};
     int summ = 0;
-    int HighestValue = 0;
+    int HighestValue = INT32_MIN;
+    int NumberOfHighestValue = 0;
     for (int i = 0; i < MassiveLenght; i++)
     {
-        if (massive[i] % 10 == 0 || massive[i] % 10 == 7 && massive[i] != 0)
+        if (massive[i] %10 == 0 || massive[i] % 10 == 7)
         {
             summ += massive[i];
         }
         if (HighestValue < massive[i])
         {
             HighestValue = massive[i];
+            NumberOfHighestValue = i;
         }
     }
     cout << "Сумма чисел, оканчивающихся цифрой 0 или 7 = " << summ << endl;
     cout << "Наибольшее число = " << HighestValue << endl;
+    cout << "Номер наибольшего числа = " << NumberOfHighestValue << endl;
+
 
     // Задание 2
     long long N;
@@ -33,19 +37,19 @@ int main() {
 
     if (N <= 0 || N >= 1000000000) {
         cout << "Число должно быть натуральным и меньше 10^9." << std::endl;
-        return 1;
+        return 1; 
     }
-    while (N > 0)
+    while (N > 0) 
     {
-        int digit = N % 10;
+        int digit = N % 10; 
         if (digit % 2 != 0)
-        {
-            sum += digit;
+        { 
+            sum += digit; 
         }
         N /= 10; // Убираем последнюю цифру из числа
     }
 
-    cout << "Сумма нечетных цифр: " << sum << std::endl;
+    std::cout << "Сумма нечетных цифр: " << sum << std::endl;
 
     return 0;
 }
